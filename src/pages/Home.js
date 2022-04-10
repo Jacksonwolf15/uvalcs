68e1b95ed8fa7b55a2201e40f19808f9213b97fe
Jackson Wolf <jtw3vhz@virginia.edu>
2022-04-10 01:58:40 -0400

    cleaned up stats tracked, added playoff bracket

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,9 +15,11 @@ function Home({teams}) {
         <div className="bg"></div>
         <div className="bg bg2"></div>
         <div className="bg bg3"></div>
-        <header>
+        <header style={{paddingBottom: "5%"}}>
           <Box align='center' round='small' pad='medium' background='rgba(255,255,255,.8)' style={{maxWidth: '40rem', margin: 'auto', borderRadius: '.25em', boxShadow:'0 0 .25em rgba(0,0,0,.25)', color: 'black', marginTop: '5%'}}>
             <h1>UVA LCS SPRING 2022</h1>
+          </Box>
+          <Box align='center' round='small' pad='medium' background='rgba(255,255,255,.8)' style={{maxWidth: '40rem', margin: 'auto', borderRadius: '.25em', boxShadow:'0 0 .25em rgba(0,0,0,.25)', color: 'black', marginTop: '5%'}}>
             <h2>STANDINGS</h2>
             <Table>
               <TableHeader>
@@ -35,6 +37,48 @@ function Home({teams}) {
                 </TableRow> 
               ))}
             </Table>
+          </Box>
+          <Box align='center' round='small' pad='medium' background='rgba(255,255,255,.8)' style={{maxWidth: '40rem', margin: 'auto', borderRadius: '.25em', boxShadow:'0 0 .25em rgba(0,0,0,.25)', color: 'black', marginTop: '5%'}}>
+            <h2>PLAYOFF BRACKET</h2>
+            <div class="split split-one">
+              <div class="round round-one current">
+                <div class="round-details"><b>Round 1</b><br/><span class="date"><b>April 17</b></span></div>
+                <ul class="matchup">
+                  <li class="team team-top">Seed 1<span class="score">0</span></li>
+                  <li class="team team-bottom">Seed 8<span class="score">0</span></li>
+                </ul>
+                <ul class="matchup">
+                  <li class="team team-top">Seed 4<span class="score">0</span></li>
+                  <li class="team team-bottom">Seed 5<span class="score">0</span></li>
+                </ul>
+                <ul class="matchup">
+                  <li class="team team-top">Seed 2<span class="score">0</span></li>
+                  <li class="team team-bottom">Seed 7<span class="score">0</span></li>
+                </ul>
+                <ul class="matchup">
+                  <li class="team team-top">Seed 3<span class="score">0</span></li>
+                  <li class="team team-bottom">Seed 6<span class="score">0</span></li>
+                </ul>												
+              </div>	
+              <div class="round round-two">
+                <div class="round-details"><b>Round 2</b><br/><span class="date"><b>April 24</b></span></div>			
+                <ul class="matchup">
+                  <li class="team team-top">&nbsp;<span class="score">&nbsp;</span></li>
+                  <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
+                </ul>	
+                <ul class="matchup">
+                  <li class="team team-top">&nbsp;<span class="score">&nbsp;</span></li>
+                  <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
+                </ul>								
+              </div>	
+              <div class="round round-three">
+                <div class="round-details"><b>Round 3</b><br/><span class="date"><b>May 1</b></span></div>			
+                <ul class="matchup">
+                  <li class="team team-top">&nbsp;<span class="score">&nbsp;</span></li>
+                  <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
+                </ul>								
+              </div>		
+            </div> 
           </Box>
         </header>
       </Grommet>
